c55407a0364543a1ec1f11a0f44b07823cf9b094
RobC <rob.clapham@gmail.com>
2022-09-08 11:09:39 +0100

    get .pre-commit working

--- src/ecgai_data_physionet_grpc/client.py
+++ src/ecgai_data_physionet_grpc/client.py
@@ -1,17 +1,18 @@
 import asyncio
-import pathlib
 import uuid
 
 from grpclib.client import Channel
 
-from definitions import ROOT_DIR
-from ecgai_data_physionet_grpc.get_by_id_service import GetByIdServiceStub, GetByIdRequest
+from ecgai_data_physionet_grpc.get_by_id_service import (
+    GetByIdRequest,
+    GetByIdServiceStub,
+)
 
 
 async def main():
     channel = Channel(host="localhost", port=45210)
     client = GetByIdServiceStub(channel)
-    item = "tests/test_data/10001_hr_request.ecgai"
+
     transaction_id = str(uuid.uuid4())
     record_id = 1
 
